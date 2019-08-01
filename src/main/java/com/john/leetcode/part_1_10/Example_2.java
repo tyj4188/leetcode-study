34ca0fdecf32e12ccb4232aa47163bcdc8d8a364
tyj4188 <617191529@qq.com>
2019-08-01 23:30:47 +0800

    Initial commit

---
+++ src/main/java/com/john/leetcode/part_1_10/Example_2.java
@@ -0,0 +1,118 @@
+/* 
+ * 
+ * 项目名：	com.john.leetcode.part_1_10
+ * 文件名：	Example_1
+ * 模块说明：	
+ * 修改历史：
+ * 2019/8/1 - Administrator - 创建。
+ */
+
+package com.john.leetcode.part_1_10;
+
+/**
+ * 题目 2
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * ---- 示例
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * @author Administrator
+ * @date 2019/8/1
+ */
+public class Example_2 {
+
+    public static void main(String[] args) {
+        ListNode param1 = createLink(new int[]{2, 4});
+        ListNode param2 = createLink(new int[]{5, 9});
+
+        ListNode result = addTwoNumbers(param1, param2);
+        System.out.println("结果: ");
+        printLink(result);
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        if(isZero(l1)) {
+            return l2;
+        }
+
+        if(isZero(l2)) {
+            return l1;
+        }
+
+        ListNode resultHead = new ListNode(0);
+        ListNode resultItr = resultHead;
+        ListNode itrA = l1;
+        ListNode itrB = l2;
+        boolean isUp = false;
+        do{
+
+            int result = nullToZero(itrA) + nullToZero(itrB);
+            result = isUp ? result + 1 : result;
+
+            if(isUp = (result >= 10)) {
+                result = result % 10;
+            }
+
+            resultItr.next = new ListNode(result);
+            resultItr = resultItr.next;
+
+            itrA = itrA != null ? itrA.next : null;
+            itrB = itrB != null ? itrB.next : null;
+        }
+        while(hasNext(itrA) || hasNext(itrB) || isUp);
+
+        return resultHead.next;
+
+    }
+
+    public static int nullToZero(ListNode node) {
+        return node == null ? 0 : node.val;
+    }
+
+    public static boolean hasNext(ListNode node) {
+        return node != null;
+    }
+
+    public static boolean isZero(ListNode node) {
+        return node == null || (!hasNext(node) && node.val == 0);
+    }
+
+    public static ListNode createLink(int[] arr) {
+        ListNode head = new ListNode(0);
+        ListNode itr = head;
+
+        for(int temp : arr) {
+            itr.next = new ListNode(temp);
+            itr = itr.next;
+        }
+
+        return head.next;
+    }
+
+    public static void printLink(ListNode head) {
+        StringBuilder sb = new StringBuilder("[");
+        ListNode itr = head;
+        while(itr != null) {
+            sb.append(itr.val).append(", ");
+            itr = itr.next;
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/main/java/com/john/leetcode/part_1_10/Example_3.java
@@ -0,0 +1,81 @@
+/* 
+ * 
+ * 项目名：	com.john.leetcode.part_1_10
+ * 文件名：	Example_3
+ * 模块说明：	
+ * 修改历史：
+ * 2019/8/1 - Administrator - 创建。
+ */
+
+package com.john.leetcode.part_1_10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 题目 3 （滑动窗口解题法）
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * ---- 示例
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ *
+ * @author Administrator
+ * @date 2019/8/1
+ */
+public class Example_3 {
+
+    public static void main(String[] args) {
+        String param1 = "abcabcbb";
+        String param2 = "bbbbb";
+        String param3 = "pwwkew";
+        String param4 = "aab";
+        String param5 = "au";
+        String param6 = "dvdf";
+        String param7 = "cdd";
+
+        System.out.println(lengthOfLongestSubstring(param1));
+        System.out.println(lengthOfLongestSubstring(param2));
+        System.out.println(lengthOfLongestSubstring(param3));
+        System.out.println(lengthOfLongestSubstring(param4));
+        System.out.println(lengthOfLongestSubstring(param5));
+        System.out.println(lengthOfLongestSubstring(param6));
+        System.out.println(lengthOfLongestSubstring(param7));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if(s == null || s.equals("")) {
+            return 0;
+        }
+
+        if(s.length() == 1) {
+            return 1;
+        }
+
+        char[] charArr = s.toCharArray();
+        if(s.length() == 2) {
+            return charArr[0] == charArr[1] ? 1 : 2;
+        }
+
+        int current, length = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int start = 0, end = 0; end < charArr.length; end++) {
+            char temp = charArr[end];
+            Integer idx = map.get(temp);
+
+            if(idx != null) {
+                start = idx > start ? idx : start;
+            }
+            current = end - start + 1;
+            length = current > length ? current : length;
+            map.put(temp, end + 1);
+
+        }
+
+        return length;
+    }
+}
