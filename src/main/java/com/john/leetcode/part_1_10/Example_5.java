f1db554f459830fc7ff80f77a0ecd3213d081788
tongyongjian <tongyongjian@olayc.cn>
2019-08-09 17:05:25 +0800

    第5题 end

--- src/main/java/com/john/leetcode/part_1_10/Example_5.java
+++ src/main/java/com/john/leetcode/part_1_10/Example_5.java
@@ -19,12 +19,43 @@ package com.john.leetcode.part_1_10;
 public class Example_5 {
 
     public static void main(String[] args) {
-        String s = "";
-        for(int start = 0, end = 0; end < s.length(); end ++) {
+        System.out.println(longestPalindrome("abc"));
+        System.out.println(longestPalindrome("aba"));
+        System.out.println(longestPalindrome("babad"));
+        System.out.println(longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome("ab"));
+        System.out.println(longestPalindrome("ac"));
+    }
+
+    public static String longestPalindrome(String s) {
+        if(s == null || s.length() == 0 || s.length() == 1) {
+            return s;
+        }
+
+        int start = 0, end = 0;
+
+        for(int i = 0; i < s.length(); i ++) {
+            int len1 = getLength(s, i, i);
+            int len2 = getLength(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            // 数量相等会覆盖前面的
+            if(len > end - start) {
+                start = i - ((len - 1) / 2);
+                end = i + (len / 2);
+            }
 
         }
 
+        return s.substring(start, end + 1);
     }
 
+    public static int getLength(String s, int l, int r) {
+        int L = l, R = r;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L --;
+            R ++;
+        }
+        return R - L - 1;
+    }
 
 }
