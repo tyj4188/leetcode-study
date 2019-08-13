16652c6f5b88e65273bc3d4f2576143925cc9f09
tongyongjian <tongyongjian@olayc.cn>
2019-08-13 18:00:35 +0800

    第15题 start

--- src/main/java/com/john/leetcode/part_11_20/Example_15.java
+++ src/main/java/com/john/leetcode/part_11_20/Example_15.java
@@ -9,6 +9,9 @@
 
 package com.john.leetcode.part_11_20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 15. 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
  *  注意：答案中不可以包含重复的三元组。
@@ -17,5 +20,15 @@ package com.john.leetcode.part_11_20;
  * @date 2019/8/13
  */
 public class Example_15 {
+    public static void main(String[] args) {
 
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+
+
+        return result;
+    }
 }
